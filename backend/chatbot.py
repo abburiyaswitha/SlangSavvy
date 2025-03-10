c244e9c073e81f01ef4381345de0b80b349cfe44
SiriReddy-1305 <sirireddykancharla1305@gmail.com>
2025-03-10 14:27:06 +0530

    Update chatbot.py

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -5,7 +5,8 @@ def get_slang_response(user_input):
         "cap": "Cap means a lie. Saying 'no cap' means you're telling the truth.",
         "bruh": "Bruh is a casual way of saying 'bro' or 'dude'.",
         "goat": "GOAT stands for 'Greatest Of All Time', used for top athletes and legends.",
-        "lit": "Lit means something is exciting, fun, or amazing!"
+        "lit": "Lit means something is exciting, fun, or amazing!",
+        "swag":"Swag refers to having a cool , confident and stylish attitude"
     }
 
     return slang_dict.get(user_input.lower(), "Sorry, I don't know that slang yet!")
